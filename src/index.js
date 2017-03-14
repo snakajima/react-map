2188ab972a77195964e784376faf420220485ffc
SATOSHI NAKAJIMA <satoshi.nakajima@gmail.com>
2017-03-14 14:21:20 -0700

    Hello World

---
+++ src/index.js
@@ -0,0 +1,13 @@
+//
+// Copyright (c) 2016 Satoshi Nakajima (https://github.com/snakajima)
+// License: The MIT License
+//
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <p>Hello World</p>,
+  document.getElementById('root')
+);
+
