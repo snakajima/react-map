faf99b32e91b0886a2978dd53c14335b6975d766
SATOSHI NAKAJIMA <satoshi.nakajima@gmail.com>
2017-03-14 14:35:45 -0700

    App.js

---
+++ src/App.js
@@ -0,0 +1,18 @@
+//
+// Copyright (c) 2016 Satoshi Nakajima (https://github.com/snakajima)
+// License: The MIT License
+//
+
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+      return <p>Hello World 2</p>;
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
 ReactDOM.render(
-  <p>Hello World</p>,
+  <App />,
   document.getElementById('root')
 );
 
